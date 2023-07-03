4a6286e2df729fdc5344d6d34515dc596421009a
Tanishq211 <malitanishq772@gmail.com>
2023-07-03 13:18:46 +0530

    did escape sequences and print statement

---
+++ Day 4.py
@@ -0,0 +1,2 @@
+print("hello world")
+print(13*3)
---
+++ Day 5.py
@@ -0,0 +1,6 @@
+print("Hello world")
+# Nice
+print("hey how you doing\nfine how about you")
+print("hey \'how\' \"you doing\"\nfine how about you")
+print("Hello",7,4, sep="-", end="059")
+print("Tanishq")
